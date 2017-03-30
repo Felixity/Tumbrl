a98ac9443e552d758355bb13fc9e33ab4ab5c7bf
LauraPrichici <p_laura_c@yahoo.com>
2017-03-29 19:33:31 -0700

    Initial revision

---
+++ Pods/Target Support Files/Pods-Tumbrl/Pods-Tumbrl-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tumbrl : NSObject
+@end
+@implementation PodsDummy_Pods_Tumbrl
+@end
---
+++ Tumbrl/PhotosViewController.swift
@@ -0,0 +1,55 @@
+//
+//  PhotosViewController.swift
+//  Tumbrl
+//
+//  Created by Laura on 3/29/17.
+//  Copyright © 2017 Laura. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+class PhotosViewController: UIViewController {
+
+    var posts: [NSDictionary] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let apiKey = "Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV"
+        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=\(apiKey)")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+        
+        let task : URLSessionDataTask = session.dataTask(with: request,completionHandler: { (dataOrNil, response, error) in
+            if let data = dataOrNil {
+                if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options:[]) as? NSDictionary {
+                    NSLog("response: \(responseDictionary)")
+                    self.posts = [responseDictionary["posts"] as! NSDictionary]
+                }
+            }
+        });
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
